e6e64afb938e395035eb7973a3e003a038e21b94
Chris Kanich <ckanich@uic.edu>
2022-09-22 22:08:16 -0500

    different option for chat

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
+import { TagCloud } from 'react-tagcloud'
+
 
 function RequestChart({ requests }) {
+  const allwords = requests.map(elt => elt.ldescription).join(" ")
+  const wordfreqs = allwords.split(/\s+/).reduce(function (map, word) {
+    map[word] = (map[word] || 0) + 1;
+    return map;
+  }, Object.create(null));
+  const wordFreqArray = Object.entries(wordfreqs).map(elt => ({ value: elt[0], count: elt[1] }))
   return (
-    <>
-    Chart goes here.
-    </>
-    
-    )
+    <div className="form-contain">
+      <TagCloud minSize={24}
+        maxSize={48}
+        tags={wordFreqArray}
+        colorOptions={{luminosity: 'dark'}}
+        className="simple-cloud"
+        randomSeed={484}
+      />  </div>
+  )
 }
 
 export default RequestChart 
